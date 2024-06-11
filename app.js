15545284488d82cdae2a156673d34447380cd53c
unknown <erkjangid2000@gmail.com>
2024-06-11 10:11:01 +0530

    first commit

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static('views'))
 
 // Server
 
-const PORT = process.env.PORT || 7000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 })
